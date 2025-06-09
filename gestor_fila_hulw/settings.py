9c23446f2ac568b9e4af37ba3a464ec235bbceac
Lucas <lucasfm.py@gmail.com>
2025-06-09 18:49:54 -0300

    arrumando url

--- gestor_fila_hulw/settings.py
+++ gestor_fila_hulw/settings.py
@@ -142,8 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-FORCE_SCRIPT_NAME = "/app"
-STATIC_URL = "/app/static/"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
